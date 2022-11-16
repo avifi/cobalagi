08f2b186643ce064c26332f1d1c4e95c8d050e3b
afifi <afifi@unisnu.ac.id>
2022-11-16 11:03:26 +0700

    view selamat

---
+++ application/views/selamat_datang.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Selamat Datang</title>
+</head>
+<body>
+
+	<h2>Selamat Datang</h2>
+
+</body>
+</html>
